a842eee80177a714d886630a6ed6df1edf3f7b8c
solygambas <51904909+solygambas@users.noreply.github.com>
2022-12-19 08:09:56 +0100

    add palindromes

--- 02-palindrome/index.js
+++ 02-palindrome/index.js
@@ -7,6 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//   const reversedString = str.split("").reverse().join("");
+//   return str === reversedString;
+// }
+
+// iterate twice, not the best solution
+function palindrome(str) {
+  return str.split("").every((char, index) => {
+    return char === str[str.length - index - 1];
+  });
+}
 
 module.exports = palindrome;
