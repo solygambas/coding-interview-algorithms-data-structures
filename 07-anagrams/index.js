9a85254e693c038e2e04a8963d5d63f6193648a2
solygambas <51904909+solygambas@users.noreply.github.com>
2022-12-21 13:56:45 +0100

    add anagrams

--- 07-anagrams/index.js
+++ 07-anagrams/index.js
@@ -8,6 +8,29 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function trim(string) {
+  return string.replace(/[^\w]/g, "").toLowerCase();
+}
+
+function mapping(string) {
+  charMap = {};
+  for (let char of string) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  return charMap;
+}
+
+function anagrams(stringA, stringB) {
+  const cleanedStringA = trim(stringA);
+  const cleanedStringB = trim(stringB);
+  const indexA = mapping(cleanedStringA);
+  const indexB = mapping(cleanedStringB);
+  const shallowCompare =
+    Object.keys(indexA).length === Object.keys(indexB).length &&
+    Object.keys(indexA).every(
+      (key) => indexB.hasOwnProperty(key) && indexA[key] === indexB[key]
+    );
+  return shallowCompare;
+}
 
 module.exports = anagrams;
